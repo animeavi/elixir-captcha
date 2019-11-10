1d3cb5086b292a1e8d13d645760d90f158f44f05
rinpatch <rinpatch@sdf.org>
2019-11-10 15:21:04 +0300

    Match on port pid when receiving messages

--- lib/captcha.ex
+++ lib/captcha.ex
@@ -1,11 +1,11 @@
 defmodule Captcha do
   # allow customize receive timeout, default: 10_000
   def get(timeout \\ 1_000) do
-    Port.open({:spawn, Path.join(:code.priv_dir(:captcha), "captcha")}, [:binary])
+    pid = Port.open({:spawn, Path.join(:code.priv_dir(:captcha), "captcha")}, [:binary])
 
     # Allow set receive timeout
     receive do
-      {_, {:data, data}} ->
+      {^pid, {:data, data}} ->
         <<text::bytes-size(5), img::binary>> = data
         {:ok, text, img }
 
